f4c0294c8ed14ee5a80365d36f853547111e4e5e
Jeff Weimer <jeffreyaweimer@gmail.com>
2016-04-20 21:27:56 -0400

    commit

--- src/components/App.vue
+++ src/components/App.vue
@@ -6,7 +6,7 @@
 					<a class="navbar-brand" href="#">Quotes!</a>
 				</div>
 				<ul class="nav navbar-nav">
-					<li class="active"><a v-link="'home'">Home</a></li>
+					<li><a v-link="'home'">Home</a></li>
 					<li><a v-link="'login'" v-if="!user.authenticated">Login</a></li>
 					<li><a v-link="'signup'" v-if="!user.authenticated">Sign Up</a></li>
 					<li><a v-link="'secretquote'" v-if="user.authenticated">Secret Quote</a></li>
